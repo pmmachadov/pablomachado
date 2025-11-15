4efdb0bea473f9a88d751a3d7f04037153a336ee
Pablo Machado <pmmachadov@gmail.com>
2025-11-15 14:07:47 +0100

    Refactor PersonalImage component to wrap img in a styled div and update personal photo asset

--- components/PersonalImage.tsx
+++ components/PersonalImage.tsx
@@ -2,14 +2,28 @@ import { ImageComponentProps } from "types";
 
 const PersonalImage: React.FC<ImageComponentProps> = ({ src = '/assets/images/personalPhoto.jpg', alt, className }) => {
     return (
-        <img
-            src={ src }
-            alt={ alt }
-            className={ className }
-            style={ {
-                width: '200px', height: '200px', objectFit: 'cover', borderRadius: '2rem', float: 'right', margin: '3rem', marginTop: 'auto'
-            } }
-        />
+        <div style={{
+            width: '200px',
+            height: '200px',
+            borderRadius: '2rem',
+            overflow: 'hidden',
+            float: 'right',
+            margin: '3rem',
+            marginTop: 'auto'
+        }}>
+            <img
+                src={ src }
+                alt={ alt }
+                className={ className }
+                style={ {
+                    width: '100%',
+                    height: '100%',
+                    objectFit: 'cover',
+                    objectPosition: 'center center',
+                    transform: 'scale(1.4)'
+                } }
+            />
+        </div>
     );
 }
 
