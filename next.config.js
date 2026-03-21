e50163e7cf9426fe43a71c21d96a37c4633e6a39
pmmachadov <pmmachadov@gmail.com>
2026-03-21 21:24:37 +0100

    Remove output config for Netlify compatibility

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
   reactStrictMode: true,
   swcMinify: true,
-  basePath: '',
   images: {
     unoptimized: true,
     domains: ['picsum.photos', 'res.cloudinary.com'],
